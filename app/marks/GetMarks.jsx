e7f9562cc73c2abee15167a493b3913000f7faa3
Lakshmanshankar <lakshmanashankrc@gmail.com>
2023-02-19 21:18:27 +0530

    finished marks and all

--- app/marks/GetMarks.jsx
+++ app/marks/GetMarks.jsx
@@ -8,6 +8,7 @@ function GetMarks() {
         axios.get('/api/user/marks')
             .then(res => {
                 SetDetails(res.data)
+                console.log(res.data)
             })
             .catch(err => {
                 console.log(err)
@@ -20,31 +21,92 @@ function GetMarks() {
             </div>
         )
     }
-    else if (details.length === 1) {
-        return (
-            <div className=' w-full'>
-                <h1>Student Marks</h1>
-                {
-                    details.map((detail) => {
-                        return (
-                            <div className='w-full'>
-                                <h1>Student Name: {detail.email}</h1>
-                                <h1>Student Graphics: {detail.graphics}</h1>
-                                <h1>Student IOT: {detail.iot}</h1>
-                                <h1>Student WebTech No: {detail.webtech}</h1>
-                                <h1>Student StLab: {detail.stlab}</h1>
-                                <h1>Student Project: {detail.project}</h1>
-                            </div>
-                        )
-                    })
-                }
-            </div>
-        )
-    }
+    // else if (details.length === 1) {
+    //     return (
+    //         <div className=' w-full'>
+    //             <h1>Student Marks</h1>
+    //             {
+    //                 details.map((detail) => {
+    //                     return (
+    //                         <div className='w-full'>
+    //                             <h1>Student Name: {detail.email}</h1>
+    //                             <h1>Student Graphics: {detail.graphics}</h1>
+    //                             <h1>Student IOT: {detail.iot}</h1>
+    //                             <h1>Student WebTech No: {detail.webtech}</h1>
+    //                             <h1>Student StLab: {detail.stlab}</h1>
+    //                             <h1>Student Project: {detail.project}</h1>
+    //                         </div>
+    //                     )
+    //                 })
+    //             }
+    //         </div>
+    //     )
+    // }
     else {
         return (
-            <div className=' w-full'>
-                <h1>Teacher Profile</h1>
+            <div className='w-full h-screen flex flex-col justify-center items-center '>
+                <div className="div w-full flex flex-col justify-center items-center">
+                    <h1>Teacher Profile</h1>
+
+                    <div className="heading flex py-0.5 rounded w-10/12 justify-between bg-fuchsia-800  ">
+                        <p className="w-1/6 bg-slate-50 m-1 p-1 font-RobotoMono text-xl font-extrabold rounded enlarge">
+                            email
+                        </p>
+                        <p className="w-1/6 bg-slate-50 m-0.5 p-1 font-RobotoMono text-xl font-extrabold rounded enlarge">
+                            Testname
+                        </p>
+                        <p className="w-1/6 bg-slate-50 m-1 p-1 font-RobotoMono text-xl font-extrabold rounded enlarge">
+                            Graphics
+                        </p>
+                        <p className="w-1/6 bg-slate-50 m-1 p-1 font-RobotoMono text-xl font-extrabold rounded enlarge">
+                            IOT
+                        </p>
+                        <p className="w-1/6 bg-slate-50 m-1 p-1 font-RobotoMono text-xl font-extrabold rounded enlarge">
+                            WebTech
+                        </p>
+                        <p className="w-1/6 bg-slate-50 m-1 p-1 font-RobotoMono text-xl font-extrabold rounded enlarge">
+                            STLab
+                        </p>
+                        <p className="w-1/6 bg-slate-50 m-1 p-1 font-RobotoMono text-xl font-extrabold rounded enlarge">
+                            Project
+                        </p>
+                        <p className="w-1/6 bg-slate-50 m-1 p-1 font-RobotoMono text-xl font-extrabold rounded enlarge">
+                            Total
+                        </p>
+                    </div>
+
+                    {details && details.map((detail) => {
+                        return (
+                            <div className="flex p-0.5 rounded w-10/12 justify-between bg-fuchsia-500 hover:transition-all hover:bg-amber-100 ">
+                                <p className="w-1/6 bg-slate-50 m-1 p-1 font-RobotoMono text-xl rounded enlarge">
+                                    {detail.email}
+                                </p>
+                                <p className="w-1/6 bg-slate-50 m-1 p-1 font-RobotoMono text-xl  rounded enlarge">
+                                    {detail.testname}
+                                </p>
+                                <p className="w-1/6 bg-slate-50 m-1 p-1 font-RobotoMono text-xl  rounded enlarge">
+                                    {detail.graphics}
+                                </p>
+                                <p className="w-1/6 bg-slate-50 m-1 p-1 font-RobotoMono text-xl rounded enlarge">
+                                    {detail.iot}
+                                </p>
+                                <p className="w-1/6 bg-slate-50 m-1 p-1 font-RobotoMono text-xl rounded enlarge">
+                                    {detail.webtech}
+                                </p>
+                                <p className="w-1/6 bg-slate-50 m-1 p-1 font-RobotoMono text-xl  rounded enlarge">
+                                    {detail.stlab}
+                                </p>
+                                <p className="w-1/6 bg-slate-50 m-1 p-1 font-RobotoMono text-xl  rounded enlarge">
+                                    {detail.project}
+                                </p>
+                                <p className="w-1/6 bg-slate-50 m-1 p-1 font-RobotoMono text-xl rounded enlarge">
+                                    {Number(detail.graphics) + Number(detail.iot) + Number(detail.webtech) + Number(detail.stlab) + Number(detail.project)}
+                                </p>
+                            </div>
+                        )
+                    }
+                    )}
+                </div>
             </div>
         )
     }
